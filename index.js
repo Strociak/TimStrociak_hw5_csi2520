4ed03747bf54195b047f392737bd13cf53f0aac3
Stroicak <Strociak@oakland.edu>
2021-11-20 16:48:13 -0500

    testo

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const ejs = require("ejs");
 // Create express app
 const app = express();
 
-// Create a database connection configuration
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "testfullstack_students", // comment out if running example 1
-});
+// // Create a database connection configuration
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root123",
+//   database: "testfullstack_students", // comment out if running example 1
+// });
 
-// Establish connection with the DB
-db.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log(`Successful connected to the DB....`);
-  }
-});
+// // Establish connection with the DB
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log(`Successful connected to the DB....`);
+//   }
+// });
 
 // Initialize Body Parser Middleware to parse data sent by users in the request object
 app.use(express.json());
@@ -35,46 +35,46 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/insertstudents", (req, res) => {
-  let data = { name: req.body.studentName, email: req.body.studentEmail };
-  let sql = `INSERT INTO students SET ?`;
-  let query = db.query(sql, data, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was inserted to the db...`);
-  });
-});
+// app.post("/insertstudents", (req, res) => {
+//   let data = { name: req.body.studentName, email: req.body.studentEmail };
+//   let sql = `INSERT INTO students SET ?`;
+//   let query = db.query(sql, data, (err, result) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(`student entry was inserted to the db...`);
+//   });
+// });
 
-app.post("/updatestudents", (req, res) => {
-  let sql = `UPDATE students SET email = '${req.body.studentNewEmailUpdate}'  WHERE id = ${req.body.studentID}`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was updated in the db...`);
-  });
-});
+// app.post("/updatestudents", (req, res) => {
+//   let sql = `UPDATE students SET email = '${req.body.studentNewEmailUpdate}'  WHERE id = ${req.body.studentID}`;
+//   db.query(sql, (err, result) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(`student entry was updated in the db...`);
+//   });
+// });
 
-app.post("/deletestudents", (req, res) => {
-  let sql = `DELETE FROM students WHERE email = '${req.body.studentEmail}'`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.send(`student entry was deleted in the db...`);
-  });
-});
+// app.post("/deletestudents", (req, res) => {
+//   let sql = `DELETE FROM students WHERE email = '${req.body.studentEmail}'`;
+//   db.query(sql, (err, result) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(`student entry was deleted in the db...`);
+//   });
+// });
 
-app.get("/readstudents", (req, res) => {
-  let sql = `SELECT * FROM students`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    res.render("readData", { data: result });
-  });
-});
+// app.get("/readstudents", (req, res) => {
+//   let sql = `SELECT * FROM students`;
+//   db.query(sql, (err, result) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.render("readData", { data: result });
+//   });
+// });
 
 // Setup server ports
 const PORT = process.env.PORT || 3000;
