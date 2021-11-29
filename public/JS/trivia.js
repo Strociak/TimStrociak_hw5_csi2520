bf0d18e3edf075b9b791bb3fbb74f91e3141c8bf
Stroicak <Strociak@oakland.edu>
2021-11-29 18:32:52 -0500

    trivia question changes

--- public/JS/trivia.js
+++ public/JS/trivia.js
@@ -64,20 +64,20 @@ var myQuestions = [
   function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
   
     function showQuestions(questions, quizContainer){
-      // we'll need a place to store the output and the answer choices
+      //store the output and the answer choices
       var output = [];
       var answers;
   
-      // for each question...
+      
       for(var i=0; i<questions.length; i++){
         
-        // first reset the list of answers
+        //list of answers
         answers = [];
   
-        // for each available answer...
+        
         for(letter in questions[i].answers){
   
-          // ...add an html radio button
+          
           answers.push(
             '<label>'
               + '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -87,28 +87,28 @@ var myQuestions = [
           );
         }
   
-        // add this question and its answers to the output
+        // add the question and its answers to the output
         output.push(
           '<div class="question">' + questions[i].question + '</div>'
           + '<div class="answers">' + answers.join('') + '</div>'
         );
       }
   
-      // finally combine our output list into one string of html and put it on the page
+      // Combine the output list into one string and place it on the html page
       quizContainer.innerHTML = output.join('');
     }
   
   
     function showResults(questions, quizContainer, resultsContainer){
       
-      // gather answer containers from our quiz
+      
       var answerContainers = quizContainer.querySelectorAll('.answers');
       
       // keep track of user's answers
       var userAnswer = '';
       var numCorrect = 0;
       
-      // for each question...
+      
       for(var i=0; i<questions.length; i++){
   
         // find selected answer
@@ -119,7 +119,7 @@ var myQuestions = [
           // add to the number of correct answers
           numCorrect++;
           
-          // color the answers green
+          // Change the color of the answers to green
           answerContainers[i].style.color = 'lightgreen';
         }
         // if answer is wrong or blank
@@ -129,14 +129,14 @@ var myQuestions = [
         }
       }
   
-      // show number of correct answers out of total
+      // show the number of correct answers out of total
       resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + ' Correct!';
     }
   
-    // show questions right away
+    // show questions
     showQuestions(questions, quizContainer);
     
-    // on submit, show results
+    // when user hits submit, show results
     submitButton.onclick = function(){
       showResults(questions, quizContainer, resultsContainer);
     }
